4213333fd1574a5f3d7709c872861aeb3776a51d
Om Prakash Prajapati <omprakash.prajapati@habilelabs.io>
2022-08-23 12:26:03 +0530

    feat: remove

--- src/app/basic-loan-info/basic-loan-info.route.js
+++
@@ -1,5 +0,0 @@
-const ctrl = require("./basic-loan-info.controller");
-
-module.exports = (router) => {
-    router.post("/api/v1/create-loan-basic-info", (_req, _res, _next) => ctrl.createBasicLoanInfo(_req).then(_res.success).catch(_next));
-};
